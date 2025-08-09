05a590ab133aef334fb7dc89924bf8ab787162af
Akshay pandurang sadaphule <138303002+Akshay12318@users.noreply.github.com>
2025-08-09 19:54:00 +0530

    Add files via upload

---
+++ Basic_python/A.Basic Loops & Numbers/Display cube of numbers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Display cube of numbers upto an Integer
+
+n=int(input("Enter a range here :"))
+
+for i in range(1,n+1):
+    print(f"Number is:{i} and cube of the {i} is :{i**3}")
+
+
+# In[ ]:
+
+
+
+
---
+++ Basic_python/A.Basic Loops & Numbers/To display natural numbers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# To display natural numbers within a given range
+
+n=int(input("Enter a range here :"))
+for i in range(1,n+1):
+      print(i)
+
+
+# In[ ]:
+
+
+
+
