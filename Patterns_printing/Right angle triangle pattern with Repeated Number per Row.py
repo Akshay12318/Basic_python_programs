10524111228b0050ee122c51612bf860f662e3e7
Akshay12318 <akshaysadaphule012@gmail.com>
2025-08-10 20:26:59 +0530

    added new files-3

---
+++ Patterns_printing/Right angle triangle pattern with Repeated Number per Row.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Right angle triangle pattern with Repeated Number per Row
+
+n=int(input("Enter a range here :"))
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end="")
+    print()
+
+
+# In[ ]:
+
+
+
+
---
+++ Patterns_printing/Right angle triangle pattern with astrisks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Right angle triangle pattern with astrisks
+
+x="*"
+n=int(input("Enter a range here :"))
+for i in range(1,n+1):
+    print(x*i)
+
+
+# In[ ]:
+
+
+
+
